22a9d41faaea46e4aedb81d8af296e6660368ae8
Adam Farhadi <adam@adamfarhadi.com>
2020-06-29 13:06:22 -0700

    2.9 done

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,10 +3,14 @@ import React, { useState } from 'react';
 const App = () =>
 {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567', id: 1 }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addPerson = (event) =>
   {
@@ -21,8 +25,7 @@ const App = () =>
 
     const newPerson = {
       name: newName,
-      number: newNumber,
-      id: persons.length + 1
+      number: newNumber
     }
 
     setPersons(persons.concat(newPerson))
@@ -40,9 +43,35 @@ const App = () =>
     setNewNumber(event.target.value)
   }
 
+  const handleNewFilter = (event) =>
+  {
+    setNewFilter(event.target.value)
+  }
+
+  const printNumbers = () =>
+  {
+    let filteredPersons = persons.filter((person) => 
+      person.name.toLowerCase().indexOf(newFilter.toLowerCase()) !== -1
+    )
+
+    return (
+      <ul>
+        {filteredPersons.map(person =>
+          <li key={person.name}>
+            {person.name} {person.number}
+          </li>
+        )}
+      </ul>
+    )
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newFilter} onChange={handleNewFilter} />
+      </div>
+      <h2>Add a new person</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNewPerson} />
@@ -55,13 +84,7 @@ const App = () =>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person =>
-          <li key={person.id}>
-            {person.name} {person.number}
-          </li>
-        )}
-      </ul>
+      {printNumbers()}
     </div>
   )
 }
