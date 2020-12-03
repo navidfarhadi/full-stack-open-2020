23f9acdcf46f3bfb60f107cc72dcd4a4c521fef0
Adam Farhadi <adam@adamfarhadi.com>
2020-12-03 13:55:18 +0100

    4.1-4.2 done, pushing initial notes app for section 4a

---
+++ part4/bloglist/backend/app.js
@@ -0,0 +1,26 @@
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
