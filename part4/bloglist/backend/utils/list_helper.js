549804912caaa2d9a432de61448280b0552d3201
Navid Farhadi <nfarhadi@kth.se>
2020-12-05 10:49:38 +0100

    4.5 done

--- part4/bloglist/backend/utils/list_helper.js
+++ part4/bloglist/backend/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blogEntry) => sum + blogEntry.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map((blog) => blog.likes)
+  const maxLikes = Math.max(...likes)
+  return blogs.find(blog => blog.likes === maxLikes)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
