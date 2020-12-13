4c944ac2b0bf949530b437c939a71c7fe7cbea62
Adam Farhadi <adam@adamfarhadi.com>
2020-12-13 14:23:52 +0100

    4.15 done

--- part4/bloglist/backend/app.js
+++ part4/bloglist/backend/app.js
@@ -2,12 +2,12 @@ const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-
 const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const usersRouter = require('./controllers/users')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -22,6 +22,8 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
