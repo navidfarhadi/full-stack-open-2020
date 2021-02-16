e945e5f78d6b3a3497c31ed989f15dc04f8af78c
Adam Farhadi <adam@adamfarhadi.com>
2021-02-16 09:44:05 +0100

    initial commit of part 5

---
+++ part5/notes/notes-backend/build/precache-manifest.81d37cff2ff14e46f79d39ea2e6c4409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6941810b37deee81477a008cc5369c01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "195c1c0a36369f452fb0",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "261379cd12f7a257a30b",
+    "url": "/static/js/2.72903e02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72903e02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "195c1c0a36369f452fb0",
+    "url": "/static/js/main.85e48a90.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
